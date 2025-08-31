15eedf369929f138d61341a0022ec0957157d406
Purushothaman2910 <velusamypurushothaman@gmail.com>
2025-08-31 15:50:56 +0530

    clent pages done

--- js/components.js
+++ js/components.js
@@ -34,6 +34,7 @@ const headerTemplate = `
         <ul class="flex items-center space-x-8">
           <li><a href="/index.html" class="nav-link">Home</a></li>
           <li><a href="/pages/about.html" class="nav-link">About</a></li>
+          <li><a href="/pages/client.html" class="nav-link">Our clients</a></li>
           <li class="relative">
             <button id="services-dropdown-btn" class="flex items-center nav-link focus:outline-none" aria-expanded="false" aria-haspopup="true">
               Services
@@ -83,6 +84,7 @@ const headerTemplate = `
       <ul class="space-y-2">
         <li><a href="/index.html" class="mobile-nav-link">Home</a></li>
         <li><a href="/pages/about.html" class="mobile-nav-link">About</a></li>
+        <li><a href="/pages/client.html" class="mobile-nav-link">Our clients</a></li>
         <li>
           <button id="mobile-services-dropdown-btn" class="w-full flex items-center justify-between mobile-nav-link text-left focus:outline-none" aria-expanded="false" aria-haspopup="true">
             Services
